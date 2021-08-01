265eb4e46bfdad3e2e15a68e78d3437275d3aeb7
Benoit Verret <benoit.verret.1988@gmail.com>
2021-08-01 13:49:45 -0400

    Threshold manipulation functionnality

--- src/spst_sensor/spst_sensor.py
+++ src/spst_sensor/spst_sensor.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from typing import Optional
 
 CREOSOTE_THRESHOLD = 0
 OVERFIRE_THRESHOLD = 0
@@ -13,3 +14,15 @@ class SPSTSensor:
         self.overfire_threshold = OVERFIRE_THRESHOLD
         self.alarm_threshold = OVERFIRE_ALARM_THRESHOLD
         self.led_states = BASE_LED_STATE
+
+    def set_threshold(
+        self, creosote_threshold: Optional[int] = None, overfire_threshold: Optional[int] = None
+    ) -> None:
+        if creosote_threshold is not None:
+            self.creosote_threshold = creosote_threshold
+        if overfire_threshold is not None:
+            self.overfire_threshold = overfire_threshold
+
+    def reset_threshold(self) -> None:
+        self.creosote_threshold = CREOSOTE_THRESHOLD
+        self.overfire_threshold = OVERFIRE_THRESHOLD
